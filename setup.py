2de7c7e9ed64051412ca67b08c87040058bc0643
roscibely <rosanacibely@gmail.com>
2022-08-30 13:44:47 -0300

    refactor: moved files

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name=' thyroid_disease_AI',
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name='thyroid_disease_AI',
     url='https://github.com/cilab-ufersa/thyroid_disease_AI',
     author='CILAB',
     author_email='rosana.rego@ufersa.edu.br',
     # Needed to actually package something
-    packages=[''],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     # Needed for dependencies
-    install_requires=[''],
+    install_requires=required,
     description='A package to predict thyroid disease',
     long_description=open('README.md').read(),
 )
