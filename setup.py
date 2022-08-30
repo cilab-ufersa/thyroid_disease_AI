f81592463a3e7db577eade5c13479b81056f5a8a
roscibely <rosanacibely@gmail.com>
2022-08-30 13:36:17 -0300

    feat: added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name=' thyroid_disease_AI',
+    url='https://github.com/cilab-ufersa/thyroid_disease_AI',
+    author='CILAB',
+    author_email='rosana.rego@ufersa.edu.br',
+    # Needed to actually package something
+    packages=[''],
+    # Needed for dependencies
+    install_requires=[''],
+    description='A package to predict thyroid disease',
+    long_description=open('README.md').read(),
+)
