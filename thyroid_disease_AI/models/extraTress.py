6b9deb2b30661ffe972e9ede89390f8a378bbb25
Caio Moisés <caio.cavalcante@alunos.ufersa.edu.br>
2023-09-19 10:39:49 -0300

    feat: Adjusted Extra Trees model

--- thyroid_disease_AI/models/extraTress.py
+++ thyroid_disease_AI/models/extraTress.py
@@ -11,8 +11,7 @@ import joblib
 if __name__ == '__main__':
 
     #Carregando o dataset
-    dataset = pd.read_csv('thyroid_disease_AI\datasets\hypothyroid\hypothyroid_dataset_clean.csv')  
-    dataset = dataset.drop_duplicates()
+    dataset = pd.read_csv('thyroid_disease_AI\datasets\hypothyroid\hypothyroid_features_final.csv')  
     output_label_dataset = dataset['binaryClass']
     dataset = dataset.drop(['binaryClass'], axis=1) 
 
@@ -22,27 +21,29 @@ if __name__ == '__main__':
     #Dividindo o dataset em treino e teste
     #80 % para treino e 20% para teste
     input_train, input_test, output_train, output_test = slipt_and_standardize_dataset(dataset_res, output_label=output_label)
+    '''
+    param_grid = {
+    'n_estimators': [50, 100, 200],           
+    'max_depth': [None, 10, 20, 30],        
+    'min_samples_split': [2, 5, 10],    
+    'min_samples_leaf': [1, 2, 4],
+    }
+    '''
+    
+    # {'max_depth': 30, 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 50}
 
-    # param_grid = {
-    # 'n_estimators': [50, 100, 200],           
-    # 'max_depth': [None, 10, 20, 30],        
-    # 'min_samples_split': [2, 5, 10],    
-    # 'min_samples_leaf': [1, 2, 4],       
-    # 'class_weight': [None, 'balanced'],
-    # 'criterion': ['gini', 'entropy'],
-    # 'random_state': [42]
-    # }
-
-    model = ExtraTreesClassifier(class_weight='balanced', 
-                                criterion='entropy',
-                                n_estimators=100, 
-                                random_state=50,
-                                max_depth = 15)
+    model = ExtraTreesClassifier(
+                                max_depth=30,
+                                min_samples_leaf=4,
+                                min_samples_split=4,
+                                n_estimators=50,
+                                    )
     model.fit(input_train, output_train)
     joblib.dump(model, 'thyroid_disease_AI\models_file\ExtraTrees.sav')
 
-    # grid_search = GridSearchCV(model, param_grid, cv=5, scoring='accuracy')
+    # grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=5, scoring='accuracy')
     # grid_search.fit(input_train, output_train) #Treinamento
+    # print(grid_search.best_params_)
 
     # Fazer a classificação 
     output_model_decision = model.predict(input_test)
